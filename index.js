dd954c3e8a41644c589cab9e7d20305e2b73f7c9
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2021-04-23 13:06:39 +0200

    Styling, show active song

--- index.js
+++ index.js
@@ -9,13 +9,19 @@ window.onload = () => {
         button.appendChild(document.createTextNode(entry.title))
         list.appendChild(button)
 
+        entry.button = button
         button.onclick = () => activate(entry)
       })
     })
 }
 
 function activate (entry) {
+  const list = document.getElementById('list')
+  Array.from(list.children).forEach(button => button.classList.remove('active'))
+
   const audio = document.getElementsByTagName('audio')[0]
   audio.src = 'data/' + entry.file
   audio.play()
+
+  entry.button.classList.add('active')
 }
